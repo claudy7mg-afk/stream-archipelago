bb7ea20705a2a944c35dc2c55be11244ea39ad26
Claudia Marginean <you@example.com>
2026-04-19 22:54:50 +0200

    attempt to fix deploy error

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Use relative asset paths so the app works on GitHub Pages project URLs.
   base: './',
+  build: {
+    target: 'esnext'
+  }
 });
